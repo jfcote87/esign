085effb61f2cc7056ab8893622703557747d82bd
Jim Cote <jfcote87@gmail.com>
2019-07-18 10:00:54 -0400

    removed test package comment.

--- v2/model/model_test.go
+++ v2/model/model_test.go
@@ -2,7 +2,7 @@
 // All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-package model_test // import "github.com/jfcote87/esign/v2/model_test"
+package model_test
 
 import (
 	"encoding/json"
